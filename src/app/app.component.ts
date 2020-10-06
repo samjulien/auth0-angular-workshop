a698737d9dc1281efd2c47fd5f63949cf79fbf66
Sam Julien <spjulien@gmail.com>
2020-10-06 14:04:01 -0700

    Fix UI flashing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public auth: AuthService) {}
+}
